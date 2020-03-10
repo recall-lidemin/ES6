26ee2613ee39587e0dc07ec78bca43497a770980
recall-lidemin <1425889927@qq.com>
2020-03-10 14:03:44 +0800

    提交

--- 002Promise.js
+++ 002Promise.js
@@ -3,6 +3,18 @@
  * 它是一个构造函数
  * 解决异步嵌套回调地狱
  * 
+ * 一个容器，保存这未来某个阶段才会结束的事件(异步操作的结果)
+ * 
+ * 两个特点：
+ *         1.对象状态不受外界影响 pending(进行中) fulfilled(已成功) rejected(已失败)
+ *         2.一旦状态改变，就不会再变，任何时候都可以得到这个结果
+ * 
+ * Promise.prototype.then(),then方法定义在原型对象上，第一个参数是resolved,第二个参数是rejected(可选)，该方法返回一个新的Promise实例
+ * 这样就可以使用链式编程
+ * Promise.prototype.catch()捕获异常，Promise状态变为rejected时调用
+ * 
+ * 
+ * 
  *  */
 
 const fs = require('fs')
